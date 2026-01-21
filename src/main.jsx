2b40bd0355ba6dd656ad515b8ad11dd3c2480b97
soubhikportfoliowebsite-droid <soubhik.portfolio.website@gmail.com>
2026-01-21 12:02:09 +0000

    update the nain

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  
-    <HashRouter>
-    <App />
-    </HashRouter>
-  
+  <App />
 );
